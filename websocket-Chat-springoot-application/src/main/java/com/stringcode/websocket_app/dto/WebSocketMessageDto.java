55da84f06e5c48b3bf8d9c95fd0d8035eda92116
Monarch-Codes00 <chinedumroster@gmail.com>
2026-02-06 15:47:17 +0930

    socket config

---
+++ websocket-Chat-springoot-application/src/main/java/com/stringcode/websocket_app/dto/WebSocketMessageDto.java
@@ -0,0 +1,43 @@
+package com.stringcode.websocket_app.dto;
+
+import com.stringcode.websocket_app.enums.MessageType;
+
+public class WebSocketMessageDto {
+    private MessageType type;
+    private String sender;
+    private String content;
+    private String room;
+
+    // Getters and Setters
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+}
